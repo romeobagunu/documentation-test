40ca5308381ca9d6a9cff03fc53cb3459d34d9ae
Romeo Bagunu <romeo.bagunu@outlook.com>
2023-03-31 17:31:29 -0500

    Remove extra period from username

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-const organizationName = "romeo.bagunu"
+const organizationName = "romeobagunu"
 const projectName = "documentation-test"
 
 /** @type {import('@docusaurus/types').Config} */
